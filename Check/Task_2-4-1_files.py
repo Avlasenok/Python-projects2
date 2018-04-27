0797275f210526ca6d7f8b7c407cacded7e355cd
Avlasenok <38257613+Avlasenok@users.noreply.github.com>
2018-04-27 23:49:34 +0300

    Don't forget about push...

--- Check/Task_2-4-1_files.py
+++ Check/Task_2-4-1_files.py
@@ -1,9 +1,10 @@
 import os
 
-path = "D:\\Downloads\\main"
+winpath = "D:\\Downloads\\main"
+# macpath = "/Users/avlasenok/Downloads/main"
 PyDirList = list()
 
-for current_dir, dirs, files in os.walk(path):
+for current_dir, dirs, files in os.walk(winpath):
     for file in files:
         if file.endswith(".py"):
             PyDirList.append(current_dir)
